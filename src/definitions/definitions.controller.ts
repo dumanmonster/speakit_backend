56ea49678f10e332f52353d078f65d63622ac4f6
dumanmonster <dybysbayd@gmail.com>
2023-05-01 18:18:28 +0600

    1123

--- src/definitions/definitions.controller.ts
+++ src/definitions/definitions.controller.ts
@@ -1,10 +1,24 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Body,
+  Post,
+  Delete,
+} from '@nestjs/common';
 import { DefinitionsService } from './definitions.service';
+import { CreateDefinitionDto } from './dto/create-definition.dto';
+import { UpdateDefinitionDto } from './dto/update-definition.dto';
 
 @Controller('definitions')
 export class DefinitionsController {
   constructor(private readonly definitionsService: DefinitionsService) {}
 
+  @Post()
+  create(@Body() createDefinitionDto: CreateDefinitionDto) {
+    return this.definitionsService.create(createDefinitionDto);
+  }
   @Get()
   findAll() {
     return this.definitionsService.findAll();
@@ -13,5 +27,17 @@ export class DefinitionsController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.definitionsService.findOne(+id);
+  }
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateDefinitionDto: UpdateDefinitionDto,
+  ) {
+    return this.definitionsService.update(+id, updateDefinitionDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.definitionsService.remove(+id);
   }
 }
--- src/definitions/definitions.service.ts
+++ src/definitions/definitions.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateDefinitionDto } from './dto/create-definition.dto';
+import { UpdateDefinitionDto } from './dto/update-definition.dto';
 
 @Injectable()
 export class DefinitionsService {
   constructor(private prisma: PrismaService) {}
+
+  create(createDefinitionDto: CreateDefinitionDto) {
+    return this.prisma.definition.create({ data: createDefinitionDto });
+  }
   findAll() {
     return this.prisma.definition.findMany();
   }
 
   findOne(id: number) {
     return this.prisma.definition.findUnique({ where: { id: id } });
+  }
+  update(id: number, updateDefinitionDto: UpdateDefinitionDto) {
+    return this.prisma.definition.update({
+      where: { id },
+      data: updateDefinitionDto,
+    });
+  }
+  remove(id: number) {
+    return this.prisma.definition.delete({ where: { id } });
   }
 }
