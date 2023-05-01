de209bd0441aaa19c643f4bcfbeee88bfa3ce2b5
dumanmonster <dybysbayd@gmail.com>
2023-05-01 20:07:59 +0600

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { DefinitionsModule } from './definitions/definitions.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, DefinitionsModule, UsersModule],
+  imports: [PrismaModule, DefinitionsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,9 +1,11 @@
-import { Role } from './../../../node_modules/.prisma/client/index.d';
 import { ApiProperty } from '@nestjs/swagger';
-import { User } from '@prisma/client';
+import { User, Role } from '@prisma/client';
 import { Exclude } from 'class-transformer';
 
 export class UserEntity implements User {
+  constructor(partial: Partial<UserEntity>) {
+    Object.assign(this, partial);
+  }
   @ApiProperty()
   id: number;
 
