f8b4be719d2e1ee1012d2d66e253338ff22f1c0e
dumanmonster <dybysbayd@gmail.com>
2023-05-01 18:18:25 +0600

    CRUD definition

--- src/definitions/dto/create-definition.dto.ts
+++ src/definitions/dto/create-definition.dto.ts
@@ -1,1 +1,11 @@
-export class CreateDefinitionDto {}
+// src/articles/dto/create-article.dto.ts
+
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateDefinitionDto {
+  @ApiProperty()
+  word: string;
+
+  @ApiProperty()
+  description: string;
+}
