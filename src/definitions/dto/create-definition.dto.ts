ff532a743b048c8afeaeaa0bea82afefa10b3866
dumanmonster <dybysbayd@gmail.com>
2023-05-03 14:26:32 +0600

    dasd

--- src/definitions/dto/create-definition.dto.ts
+++ src/definitions/dto/create-definition.dto.ts
@@ -1,17 +1,25 @@
 // src/articles/dto/create-article.dto.ts
 
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class CreateDefinitionDto {
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty()
   word: string;
 
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty()
   description: string;
 
+  @IsString()
+  @Length(2, 2)
   @ApiProperty()
   level: string;
 
+  @IsString()
   @ApiProperty()
   topicId: string;
 }
