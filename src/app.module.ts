20e2e47bb6d3ea6e3e0b6f5faf516ffc74c6da5a
dumanmonster <dybysbayd@gmail.com>
2023-05-12 19:01:04 +0600

    fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { DefinitionsModule } from './definitions/definitions.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TopicsModule } from './topics/topics.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [DefinitionsModule, UsersModule, AuthModule, TopicsModule],
